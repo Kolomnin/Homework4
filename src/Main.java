57f6a9aebd85d2c5a96b954335200b197a5083a9
Kolomnin <kolomninsp@gmail.com>
2022-08-08 22:09:06 +0300

    первый коммит ДЗ4

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    //Домашнее задание №4
+    // Задание 1
+    public static void task1() {
+
+    }
+}
